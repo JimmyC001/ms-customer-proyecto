96f9fb9261c2f6693bdd0f721083181693087650
Jimmy Castro <ja.castros@javeriana.edu.co>
2023-05-27 15:45:21 -0500

    First commit

---
+++ src/main/java/co/com/cattleya/ms/customer/MsCustomerProyectoApplication.java
@@ -0,0 +1,13 @@
+package co.com.cattleya.ms.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustomerProyectoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCustomerProyectoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/cattleya/ms/customer/MsCustomerProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.cattleya.ms.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomerProyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
